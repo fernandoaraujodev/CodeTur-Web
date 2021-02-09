3e87ef82e886306c76f9eb89b9e7d069abb5e342
Fernando Araujo <fernandovcaraujo@gmail.com>
2021-02-09 00:16:30 -0300

    Trabalhando com Formik no Login

--- codetur-web/src/pages/conta/login/index.jsx
+++ codetur-web/src/pages/conta/login/index.jsx
@@ -8,7 +8,6 @@ import Menu from '../../../components/menu';
 //libs
 import {useFormik} from 'formik';
 import {Container, Form, Button} from 'react-bootstrap';
-import ResetarSenha from '../resetarsenha';
 
  const Login = () => {
 
@@ -22,31 +21,45 @@ import ResetarSenha from '../resetarsenha';
             senha : ''
         },//e suas ações
         onSubmit : values => {
-            alert(values);
+            alert(JSON.stringify(values));
         }
     });
 
     return (
         <div>
             <Menu />
-            <Container className='form-height'> /* */
+            <Container className='form-height'> {/* quando houver uma ação SUBMIT pegar dados do formik */}
                 <Form className='form-signin' onSubmit={formik.handleSubmit} >
                     <h1>Login</h1>
                     <small>Informe os dados Abaixo</small>
                         
                     <hr/>
-                    <Form.Group controlId="formBasicEmail">
+                    <Form.Group>
                         <Form.Label>Email </Form.Label>
-                        <Form.Control type="email" placeholder="Informe o email" required />
+                        <Form.Control 
+                        type="email" 
+                        name="email"
+                        placeholder="Informe o email" 
+                        value={formik.values.email} 
+                        onChange={formik.handleChange}
+                        required />
                     </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
+                    <Form.Group>
                         <Form.Label>Senha</Form.Label>
-                        <Form.Control type="password" placeholder="Senha"   required/>
+                        <Form.Control 
+                        type="password" 
+                        name="senha"
+                        placeholder="Senha" 
+                        value={formik.values.senha} 
+                        onChange={formik.handleChange}
+                        required/>
                     </Form.Group>
+
                     <Button variant="primary" type="submit">
                         Enviar
                     </Button>
+                    
                     <br/><br/>
                     <a href='/conta/resetar-senha' style={{ marginTop :'30px'}}>Esqueci a senha!</a>
                 </Form>
