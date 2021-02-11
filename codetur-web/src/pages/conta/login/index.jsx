996807127631d046755f4ed16a7339655466b152
Fernando Araujo <fernandovcaraujo@gmail.com>
2021-02-11 17:14:19 -0300

    Erro com o toast solucionado

--- codetur-web/src/pages/conta/login/index.jsx
+++ codetur-web/src/pages/conta/login/index.jsx
@@ -16,97 +16,85 @@ import ContaServico from '../../../services/contaServico';
 
 const Login = () => {
 
-    const { addToast } = useToasts();
+    // const [email, setEmail] = useState('');
+   // const [senha, setSenha] = useState('');
 
-    const history = useHistory();
+   const history = useHistory();
+   const { addToast } = useToasts();
+   const formik = useFormik({
+       initialValues :{
+           email : '',
+           senha : ''
+       },
+       onSubmit : values => {
+           ContaServico
+               .logar(values)
+               .then(resultado => resultado.json())
+               .then(resultado => {
+                   if(resultado.sucesso){
+                       //apresenta notificação
+                       addToast(resultado.data.mensagem, {
+                           appearance: 'success',
+                           autoDismiss: true,})
+                       //salva token localstorage
+                       localStorage.setItem('token-codetur', resultado.data.token);
+                       //redireciona pagina admin
+                       history.push('/admin');
+                   } else {
+                       addToast(resultado.data.mensagem, {
+                           appearance: 'error',
+                           autoDismiss: true,
+                       })
+                   }
+               
+               })
+               .catch(erro => {
+                   console.error('erro na api ' + erro);
+               })
+       
+       }
+   })
 
-    //implementando o Formik - yarn add formik
-    //gerenciamento do formulario + valores
-    const formik = useFormik({
-        
-        //todo formulario tem suas entradas
-        initialValues: {
-            email : '',
-            senha : ''
-        },//e suas ações
-        onSubmit : values => {
-            
-            //alert(JSON.stringify(values)); confirmar se esta enviando
-            
-            //implementado os services
-            ContaServico
-                .logar(values)
-                .then(resultado => resultado.json())
-                .then(resultado => {
-                    if(resultado.sucesso){
-                        
-                        //apresenta notificação
-                        //alert(resultado.mensagem)
-                        addToast(resultado.message, {appearance : 'sucess'}, true);
-                        
-                        //salva o token no localStorage
-                        localStorage.setItem('token-codetur', resultado.data.token);
-                        
-                        //redireciona
-                        history.push('/admin');
-                    }
-                    else{
-                        
-                        //apresenta notificação
-                        //alert(resultado.mensagem)
-                        addToast(resultado.message, {appearance : 'error'}, true);
-                    }
-                })
+   return (
+       <div>
+           <Menu />
+           <Container className='form-height'>
+               <Form className='form-signin' onSubmit={formik.handleSubmit}>
+                   <h1>Login</h1>
+                   <small>Informe os dados Abaixo</small>
+                       
+                   <hr/>
+                   <Form.Group >
+                       <Form.Label>Email </Form.Label>
+                       <Form.Control type="email" 
+                                    name="email"
+                                    placeholder="Informe o email" 
+                                    value={formik.values.email} 
+                                    onChange={formik.handleChange} 
+                                    required />
+                       
+                       
+                   </Form.Group>
 
-                //caso ocorra um erro
-                .catch(erro => {
-                    console.error("erro na api" + erro);
-                })
-        }
-    });
-
-    return (
-        <div>
-            <Menu />
-            <Container className='form-height'> {/* quando houver uma ação SUBMIT, pegar dados do formik */}
-                <Form className='form-signin' onSubmit={formik.handleSubmit} >
-                    <h1>Login</h1>
-                    <small>Informe os dados Abaixo</small>
-                        
-                    <hr/>
-                    <Form.Group>
-                        <Form.Label>Email </Form.Label>
-                        <Form.Control 
-                        type="email" 
-                        name="email"
-                        placeholder="Informe o email" 
-                        value={formik.values.email} 
-                        onChange={formik.handleChange}
-                        required />
-                    </Form.Group>
-
-                    <Form.Group>
-                        <Form.Label>Senha</Form.Label>
-                        <Form.Control 
-                        type="password" 
-                        name="senha"
-                        placeholder="Senha" 
-                        value={formik.values.senha} 
-                        onChange={formik.handleChange}
-                        required/>
-                    </Form.Group>
-
-                    <Button variant="primary" type="submit">
-                        Enviar
-                    </Button>
-                    
-                    <br/><br/>
-                    <a href='/conta/resetar-senha' style={{ marginTop :'30px'}}>Esqueci a senha!</a>
-                </Form>
-            </Container>
-            <Rodape />
-        </div>
-    )
+                   <Form.Group>
+                       <Form.Label>Senha</Form.Label>
+                       <Form.Control type="password" 
+                                    name="senha"
+                                    placeholder="Senha"   
+                                    value={formik.values.senha}
+                                    onChange={formik.handleChange}
+                                    required/>
+                   </Form.Group>
+                   <Button variant="primary" type="submit">
+                       Enviar
+                   </Button>
+                   <br/><br/>
+                   <a href='/resetar-senha' style={{ marginTop :'30px'}}>Esqueci a senha!</a>
+               </Form>
+           </Container>
+           <Rodape />
+       </div>
+   )
 
 }
 
